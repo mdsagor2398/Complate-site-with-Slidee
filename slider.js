54a2afe45114d8226d9d719c398904c9f1803e9d
mdsagor2398 <mdsagorhossain2398@gmail.com>
2024-03-11 00:49:28 +0600

    new style and css add

--- slider.js
+++ slider.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
-    $(".hero_slider").owlCarousel({
+    $(".hero_slider ").owlCarousel({
         loop: true,
         margin: 10,
         responsiveClass: true,
         autoplay: 50,
         nav: false,
         center: true,
-        items: 2,
+        items: 3,
         responsive: {
             0: {
                 items: 3,
@@ -18,6 +18,39 @@ $(document).ready(function () {
             },
             1000: {
                 items: 2,
+                nav: false,
+                margin: 30,
+            },
+        },
+    });
+});
+
+
+
+
+
+
+
+
+$(document).ready(function () {
+    $(".testimonial ").owlCarousel({
+        loop: true,
+        margin: 10,
+        responsiveClass: true,
+        autoplay: 50,
+        nav: false,
+        
+        responsive: {
+            0: {
+                items: 3,
+                nav: false,
+            },
+            600: {
+                items: 5,
+                nav: false,
+            },
+            1000: {
+                items: 4,
                 nav: false,
                 margin: 30,
             },
